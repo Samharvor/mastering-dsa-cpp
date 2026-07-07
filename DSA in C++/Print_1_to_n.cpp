4abef3af322092c262ea6ba542b574c0e3c71fb1
Samharvor <theshivthakur@gmail.com>
2026-07-07 19:55:50 +0530

    Practice Question of while loop

---
+++ DSA in C++/Print_1_to_n.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+
+    cout << "Enter the Number: ";
+    cin >> n;
+
+    int i = 0;
+    while(i <= n ){
+        cout << i << endl;
+        i++;
+    }
+
+    return 0;
+}
